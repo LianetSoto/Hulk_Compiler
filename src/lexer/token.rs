0baa3b8788f70c7ddfbd46aee35ab29fe40f758e
Bazan49 <aixabazan0705@gmail.com>
2026-04-15 15:07:51 -0400

    feat(lexer): add new tokens

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,16 +1,50 @@
 use logos::Logos;
 
 #[derive(Debug, Logos, PartialEq)]
-#[logos(skip r"[ \t\n\f]+")] // Ignore this regex pattern between tokens
+#[logos(skip r"[ \t\n\f]+")] // Ignora este patrón entre tokens
 pub enum Token {
 
     // Keywords
-    #[token("print")]  
-    Print,
+    #[token("print")] Print,
+    #[token("function")] Function,
+    #[token("let")] Let,
+    #[token("in")] In,
+    #[token("if")] If,
+    #[token("else")] Else,
+    #[token("elif")] Elif,
+    #[token("while")] While,
+    #[token("for")] For,
+
+    // 
+    #[token("(")]  LParen,
+    #[token(")")]  RParen,
+    #[token("{")]  LBrace,
+    #[token("}")]  RBrace,
 
     // Operators 
-    #[token("+")]
-    Plus,
+    #[token("+")]  Plus,
+    #[token("-")]  Minus,
+    #[token("*")]  Mult,
+    #[token("/")]  Div,
+    #[token("^")]  Power,
+    #[token(".")]  Dot,
+    #[token(",")]  Comma,
+    #[token(":")]  Colon,
+    #[token(";")]  Semicolon,
+    #[token("@")]  Concat,
+
+    // revisar
+    #[token(":=")] Assign,
+    #[token("=>")] Arrow,
+    #[token("==")] EqEq,
+    #[token("!=")] Neq,
+    #[token("<")]  Lt,
+    #[token(">")]  Gt,
+    #[token("<=")] Leq,
+    #[token(">=")] Geq,
+    #[token("&")]  And,
+    #[token("|")]  Or,
+    #[token("!")]  Not,
 
     // Regular expression patterns
     #[regex(r"[0-9]+(\.[0-9]+)?", |lex| lex.slice().parse::<f64>().ok())]
